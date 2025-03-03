53550cd9495c7b558da7c5cc700a9690d0f514c0
SteveenR834 <sfrevelo@udenar.edu.co>
2025-03-02 21:59:54 -0500

    Versión 1 - Basic Calculator

---
+++ calculator.py
@@ -0,0 +1,26 @@
+
+continuar = "y"
+resultado= 0
+while continuar=="y"  or continuar=="y":
+
+    n1 = int(input("Ingrese el primer dígito: "))
+    n2 = int(input("Ingrese el segundo dígito: "))
+    op = int(input("Elija la operación a realizar según el numeral"
+          "\n1) Suma"
+          "\n2) Resta"
+          "\n3) Multiplicación"
+          "\n4) División\n")
+          )
+    
+    if (op==1):
+        print(f"{n1}+{n2}={n1+n2}")
+    elif(op==2):
+        print(f"{n1}-{n2}={n1-n2}")
+    elif(op==3):
+        print(f"{n1}*{n2}={n1*n2}")
+    elif(op==4):
+        print(f"{n1}/{n2}={n1/n2}")
+    else:
+        print("La operación seleccionada no es válida")
+
+    continuar = input("Desea realizar otra operación? (Y/N)")
